7c5659ba3f61829cc73083b7493dfc2e921c4c48
Ricardo Oyarzun <royarzun@gmail.com>
2020-03-05 10:44:53 -0300

    chore: add testing settings

--- books/settings/testing.py
+++ books/settings/testing.py
@@ -2,8 +2,11 @@
 from .defaults import *
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'test.sqlite3')
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
