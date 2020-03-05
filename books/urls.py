e71dbdec092a617868695ce711402bfee6504a39
Ricardo Oyarzun <royarzun@gmail.com>
2020-03-04 23:25:35 -0300

    fix(books.urls): add shelves routes

--- books/urls.py
+++ books/urls.py
@@ -7,20 +7,25 @@ from drf_yasg import openapi
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Bookshelf API",
-      default_version='v1',
-      description="A very simple bookshelf API",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="royarzun@gmail.com"),
-      license=openapi.License(name="MIT License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Bookshelf API",
+        default_version="v1",
+        description="A very simple bookshelf API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="royarzun@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('', include('social_django.urls', namespace='social')),
-   path('api/', include('apps.api.urls')),
-   path('auth/', include('apps.auth.urls'))
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("", include("social_django.urls", namespace="social")),
+    path("", include("apps.shelves.urls", namespace="shelves")),
+    path("api/", include("apps.api.urls")),
+    path("auth/", include("apps.auth.urls")),
 ]
