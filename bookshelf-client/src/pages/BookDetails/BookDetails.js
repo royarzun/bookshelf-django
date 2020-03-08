5de3840e961510cf87fbaea9e10c81970e722536
Rafael Poveda <rafael.poveda@cornershopapp.com>
2020-03-08 15:41:07 -0300

    feat: add Book details page

---
+++ bookshelf-client/src/pages/BookDetails/BookDetails.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import WithHeader from "../../layouts/WithHeader";
+import Book from "../../components/Book";
+import { getBooks } from "../../services/api";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getBooks()
+      .then(res => {
+        setLoading(false);
+        setBooks(res.books);
+      })
+      .catch(err => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <WithHeader>
+      <ul className="max-w-screen-xl mx-auto py-10 flex flex-wrap justify-center">
+        {!isLoading
+          ? books.map(book => (
+              <li key={book.id} className="w-1/2 md:w-1/4">
+                <Book {...book} />
+              </li>
+            ))
+          : [...new Array(16)].map((_book, index) => (
+              <li key={index} className="w-1/2 md:w-1/4">
+                <Book isPlaceholder />
+              </li>
+            ))}
+      </ul>
+    </WithHeader>
+  );
+};
+
+export default Books;
